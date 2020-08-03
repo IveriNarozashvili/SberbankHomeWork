23d5f375fa2547b518a7020106683374b3ecd095
IveriNarozashvili <geobmw91>
2020-08-03 20:19:41 +0300

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,20 +3,23 @@ pipeline {
     agent any
 
     stages {
-        stage('Очистка') {
-            cleanWs()
-        }
         stage('Build process') {
-            withMaven(jdk: 'jdk', maven: 'maven') {
-                // some block
-                sh "mvn clean test -Dtags=${PARAM}"
+            steps{
+                withMaven(jdk: 'jdk', maven: 'maven') {
+                    // some block
+                    bat "mvn clean test -Dtags=${PARAM}"
+                }
             }
+
         }
         stage('Allure report generation') {
-            allure includeProperties: false,
-                    jdk: 'jdk',
-                    report: 'target/allure-report',
-                    results: [[path: 'target/allure-results']]
-        }
+            steps{
+                allure includeProperties: false,
+                        jdk: 'jdk',
+                        report: 'target/allure-report',
+                        results: [[path: 'target/allure-results']]
+            }
+            }
+
     }
 }
